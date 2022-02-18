1822c97f0494dbd15b91bffbd5fb46ec3f1a4201
Christian Müller <dev@c-mueller.xyz>
2022-02-18 17:02:24 +0100

    Added Option to use HandlerFunc with the api

---
+++ faas_webhook.go
@@ -0,0 +1,38 @@
+package tgbotapi
+
+import (
+	"encoding/json"
+	"net/http"
+)
+
+func (bot *BotAPI) GetHandlerFuncForWebhook(onUpdate func(update *Update) error) http.HandlerFunc {
+	return func(res http.ResponseWriter, req *http.Request) {
+		defer func() {
+			if r := recover(); r != nil {
+				res.WriteHeader(http.StatusInternalServerError)
+				res.Header().Set("Content-Type", "application/json")
+				errMsg, _ := json.Marshal(map[string]string{"error": "Paniced. See Server logs."})
+				_, _ = res.Write(errMsg)
+				log.Println("Recovered from panic in webhook handler func. Error: ", r)
+			}
+		}()
+		update, err := bot.HandleUpdate(req)
+		if err != nil {
+			errMsg, _ := json.Marshal(map[string]string{"error": err.Error()})
+			res.WriteHeader(http.StatusBadRequest)
+			res.Header().Set("Content-Type", "application/json")
+			_, _ = res.Write(errMsg)
+			return
+		}
+
+		err = onUpdate(update)
+		if err != nil {
+			log.Printf("Failed to handle update. onUpdate Returned %q", err.Error())
+			errMsg, _ := json.Marshal(map[string]string{"error": err.Error()})
+			res.WriteHeader(http.StatusInternalServerError)
+			res.Header().Set("Content-Type", "application/json")
+			_, _ = res.Write(errMsg)
+			return
+		}
+	}
+}
